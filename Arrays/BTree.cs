5ee2845c4adbfcf0f4fb772af5ae4ac59a0ba684
Luviz <bardiajedi@gmail.com>
2015-09-06 12:10:46 +0200

    1st upload to github.com

---
+++ Arrays/BTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays {
+	class BTree<T> {
+
+		Node<T> root;
+		Node<T> walker;
+
+	}
+}
